09000f7982e5522e00e2c610f64c3ae5cdcbdc2e
Ryan Day <soldair@gmail.com>
2011-10-23 11:48:24 -0700

    tests for validations

--- tests.sh
+++ tests.sh
@@ -1,11 +1,19 @@
+cd `dirname $0`
 if [ "$1" == "-c" ]
 then
 	rm -f lib/*~
-	expresso -I lib-cov -c tests/* $2
+	rm -f app/*~
+	export EXPRESSO_TEST_PATH=`pwd`/coverage
+	mkdir -p $EXPRESSO_TEST_PATH
+	# i dont know what happened but for some reson expresso stoped unshifting -I or --include onto require.paths
+	node-jscoverage lib coverage/lib
+	node-jscoverage app coverage/app
+	expresso -I coverage -c tests/* $2
 	export pass=$?
-	rm -fr lib-cov
+	rm -fr $EXPRESSO_TEST_PATH
 	exit $pass
 else
+	export EXPRESSO_TEST_PATH=`pwd`
 	expresso -I lib tests/*
 	exit
 fi
--- tests/validate.js
+++ tests/validate.js
@@ -1,15 +1,25 @@
-var valid = require('../lib/validate.js')
+if(process.env.EXPRESSO_TEST_PATH) require.paths.unshift(process.env.EXPRESSO_TEST_PATH);
+
+var valid = require('lib/validate.js')
 ,should = require('should');
 
 module.exports  = {
 	validEmail:function(){
 		//valid email should be valid
-		valid.email('soldair@gmail.com').should.eql(true);
-
+		valid.email('soldair@gmail.com').should.be.true;
 		//valid email with no tld chunk should not be considered valid
-		valid.email('soldair@gmail').should.eql(false);
-
+		valid.email('soldair@gmail').should.be.false;
 		//invalid email should be invalid
-		valid.email('soldair').should.eql(false);
+		valid.email('soldair').should.be.false;
 	}
-}
+	,validName:function(){
+		valid.name('ryan').should.be.true;
+		valid.name('').should.be.false;
+		valid.name('  ').should.be.false;
+	}
+	,validPass:function(){
+		valid.password('aaaaaa').should.be.true;
+		valid.password('12345').should.be.false;
+		valid.password('').should.be.false;
+	}
+};
